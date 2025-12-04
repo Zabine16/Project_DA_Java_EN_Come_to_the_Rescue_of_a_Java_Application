5bd1a49fabdba11b77b6eee4f6e0c3950be10c42
Zabine <zabine5678@gmail.com>
2025-12-04 16:08:44 +0400

    Etape 5 - Main Implementation

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,7 +1,5 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,12 +10,12 @@ public class AnalyticsCounter {
     private ISymptomWriter writer;
     private ISymptomReader reader;
 
-    public AnalyticsCounter(ISymptomWriter writer, ISymptomReader reader){
-        this.writer=writer;
-        this.reader=reader;
+    public AnalyticsCounter(ISymptomWriter writer, ISymptomReader reader) {
+        this.writer = writer;
+        this.reader = reader;
     }
 
-    public List<String> getSymptoms(){
+    public List<String> getSymptoms() {
         return reader.GetSymptoms();
     }
 
@@ -26,41 +24,23 @@ public class AnalyticsCounter {
     }
 
 
-    public Map<String, Integer> countSymptoms(List<String> listOfSymptoms){
+    public Map<String, Integer> countSymptoms(List<String> listOfSymptoms) {
         Map<String, Integer> symptomCount = new HashMap<>();
 
-        for(String symptom : listOfSymptoms){
-            if(symptomCount.containsKey(symptom)){
+        for (String symptom : listOfSymptoms) {
+            if (symptomCount.containsKey(symptom)) {
                 symptomCount.put(symptom, symptomCount.get(symptom) + 1);
-            }
-            else{
+            } else {
                 symptomCount.put(symptom, 1);
             }
         }
         return symptomCount;
     }
 
-    public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms){
+    public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
         return new TreeMap<>(symptoms);
     }
 
 
 
-    public static void main(String args[]) throws Exception {
-
-        ISymptomWriter writer = new WriteSymptomDataToFile();
-
-        ISymptomReader reader = new ReadSymptomDataFromFile("Project02Eclipse/src/com/hemebiotech/analytics/symptoms.txt");
-
-        AnalyticsCounter counter = new AnalyticsCounter(writer, reader);
-
-        List<String> symptoms = counter.getSymptoms();
-
-        Map<String, Integer> countedSymptoms = counter.countSymptoms(symptoms);
-
-        Map<String, Integer> sortedSymptoms = counter.sortSymptoms(countedSymptoms);
-
-        counter.writeSymptoms(sortedSymptoms);
-
-    }
 }
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -0,0 +1,25 @@
+package com.hemebiotech.analytics;
+
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+
+        ISymptomWriter writer = new WriteSymptomDataToFile();
+
+        ISymptomReader reader = new ReadSymptomDataFromFile("Project02Eclipse/src/com/hemebiotech/analytics/symptoms.txt");
+
+        AnalyticsCounter counter = new AnalyticsCounter(writer, reader);
+
+        List<String> symptoms = counter.getSymptoms();
+
+        Map<String, Integer> countedSymptoms = counter.countSymptoms(symptoms);
+
+        Map<String, Integer> sortedSymptoms = counter.sortSymptoms(countedSymptoms);
+
+        counter.writeSymptoms(sortedSymptoms);
+
+        System.out.println("Symptoms successfully added to result.out");
+    }
+}
