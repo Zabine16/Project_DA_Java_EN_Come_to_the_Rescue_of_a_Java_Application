1316092f6f3d523c94bb8731fa0375e3ddf762ba
Zabine <zabine5678@gmail.com>
2025-12-04 15:52:12 +0400

    Etape 4 - Refactorer AnalyticsCounter en plusieurs methodes

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,40 +3,64 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-    private static int headacheCount = 0;
-    private static int rashCount = 0;
-    private static int pupilCount = 0;
+
+    private ISymptomWriter writer;
+    private ISymptomReader reader;
+
+    public AnalyticsCounter(ISymptomWriter writer, ISymptomReader reader){
+        this.writer=writer;
+        this.reader=reader;
+    }
+
+    public List<String> getSymptoms(){
+        return reader.GetSymptoms();
+    }
+
+    public void writeSymptoms(Map<String, Integer> symptoms) {
+        writer.writeSymptoms(symptoms);
+    }
+
+
+    public Map<String, Integer> countSymptoms(List<String> listOfSymptoms){
+        Map<String, Integer> symptomCount = new HashMap<>();
+
+        for(String symptom : listOfSymptoms){
+            if(symptomCount.containsKey(symptom)){
+                symptomCount.put(symptom, symptomCount.get(symptom) + 1);
+            }
+            else{
+                symptomCount.put(symptom, 1);
+            }
+        }
+        return symptomCount;
+    }
+
+    public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms){
+        return new TreeMap<>(symptoms);
+    }
+
+
 
     public static void main(String args[]) throws Exception {
 
-        BufferedReader reader = new BufferedReader(new FileReader("Project02Eclipse/src/com/hemebiotech/analytics/symptoms.txt"));
-        String line = reader.readLine();
-
-        int headCount = 0;
-        while (line != null) {
-            System.out.println("symptom from file: " + line);
-            if (line.equals("headache")) {
-                headCount++;
-                System.out.println("number of headaches: " + headCount);
-            } else if (line.equals("rash")) {
-                rashCount++;
-            } else if (line.contains("pupils")) {
-                pupilCount++;
-            }
-
-            line = reader.readLine();
-        }
-        reader.close();
-
         ISymptomWriter writer = new WriteSymptomDataToFile();
-        Map<String, Integer> symptoms = new HashMap<>();
-        symptoms.put("headache", headCount);
-        symptoms.put("rash", rashCount);
-        symptoms.put("dialated pupils", pupilCount);
 
-        writer.writeSymptoms(symptoms);
+        ISymptomReader reader = new ReadSymptomDataFromFile("Project02Eclipse/src/com/hemebiotech/analytics/symptoms.txt");
+
+        AnalyticsCounter counter = new AnalyticsCounter(writer, reader);
+
+        List<String> symptoms = counter.getSymptoms();
+
+        Map<String, Integer> countedSymptoms = counter.countSymptoms(symptoms);
+
+        Map<String, Integer> sortedSymptoms = counter.sortSymptoms(countedSymptoms);
+
+        counter.writeSymptoms(sortedSymptoms);
+
     }
 }
